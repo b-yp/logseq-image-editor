6bd28b991bd307ff8ee22cb9f9a6a29430ec59c6
b-yp <bypl@vip.qq.com>
2023-08-28 23:49:03 +0800

    feat: Support for network images

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,33 @@ function App() {
       if (!text) return;
 
       const imageRegex = /!\[.*?\]\((.*?)\)/g;
+      const urlRegex =
+        /(https?:\/\/[^\s]*\.(png|jpg|jpeg|gif|bmp|webp|mp3|wav|ogg|mp4|mov|avi|wmv|flv|pdf))([^\s(){}]*)/gi;
       const matches = text.match(imageRegex);
+      const urls = text.match(urlRegex);
+
+      if (urls && urls.length) {
+        console.log("urll", urls[0]);
+        setImageUrl(urls[0]);
+
+        logseq.showMainUI();
+        return;
+      }
 
       if (matches && matches.length) {
         const imageUrls = matches.map(
           (match) =>
             `${path}${(match.match(/!\[.*?\]\((.*?)\)/) || [])[1].substring(2)}`
         );
+
         setImageUrl(imageUrls[0]);
         logseq.showMainUI();
-      } else {
-        logseq.hideMainUI();
-        logseq.UI.showMsg("No image URLs found.", "warning");
-        console.log("No image URLs found.");
+        return;
       }
+
+      logseq.hideMainUI();
+      logseq.UI.showMsg("No image URLs found.", "warning");
+      console.log("No image URLs found.");
     });
   };
 
